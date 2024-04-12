63648660642a42644b2456cd5eaa1583f113ddc2
JARABRAMA <brayanjaraba2006@gmail.com>
2024-04-12 18:56:04 -0500

    save

--- src/main/java/culturemedia/exception/DurationNotValidException.java
+++ src/main/java/culturemedia/exception/DurationNotValidException.java
@@ -1,7 +1,9 @@
 package culturemedia.exception;
 
+import java.text.MessageFormat;
+
 public class DurationNotValidException extends CulturetecaException{
-    public DurationNotValidException(String message) {
-        super(message);
+    public DurationNotValidException(String title, double duration) {
+        super(MessageFormat.format("the title {0} with duratio {1} was not found", title, duration));
     }
 }
