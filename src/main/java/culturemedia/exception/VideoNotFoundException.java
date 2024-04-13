a65b825c99e0cafabf4e662c2413c79b5126dab2
JARABRAMA <brayanjaraba2006@gmail.com>
2024-04-12 19:19:20 -0500

    messages of exceptions fixed

--- src/main/java/culturemedia/exception/VideoNotFoundException.java
+++ src/main/java/culturemedia/exception/VideoNotFoundException.java
@@ -1,7 +1,12 @@
 package culturemedia.exception;
 
+import java.text.MessageFormat;
+
 public class VideoNotFoundException extends CulturetecaException{
-    public VideoNotFoundException(String message) {
-        super(message);
+    public VideoNotFoundException(String title) {
+        super(MessageFormat.format("Video not found: {0}", title));
+    }
+    public VideoNotFoundException() {
+        super("any video not found");
     }
 }
