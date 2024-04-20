6f69cc7f953e58af9b979f4531ea20844be6fae9
JARABRAMA <brayanjaraba2006@gmail.com>
2024-04-20 06:05:33 -0500

    I have iproved the testing of the service

--- src/main/java/culturemedia/repository/impl/VideoRepositoryImpl.java
+++ src/main/java/culturemedia/repository/impl/VideoRepositoryImpl.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VideoRepositoryImpl implements VideoRepository {
-    private static final List<Video> videos = new ArrayList<Video>();
+    private static final List<Video> videos = new ArrayList<>();
 
     @Override
     public List<Video> findAll() {
@@ -24,7 +24,7 @@ public class VideoRepositoryImpl implements VideoRepository {
 
     @Override
     public List<Video> find(String title) {
-        return findAll().stream().filter(video -> video.title().equals(title)).toList();
+        return findAll().stream().filter(video -> video.title().contains(title)).toList();
     }
 
     @Override
